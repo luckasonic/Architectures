4b0d2397c3e828765b36116f2aef99118f393382
luckasonic <151017044+luckasonic@users.noreply.github.com>
2024-03-15 01:27:03 +0200

    Full high-level implementation

--- src/Main.java
+++ src/Main.java
@@ -4,78 +4,51 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> indexes = new ArrayList<>();
-        String pidStr = DataInput.getString("Введіть шукану підстрічку: ").toLowerCase();
-        while (true) {
-            int choice = DataInput.getInt("1 - Ввести стрічку \n2 - Вийти і вивести кількість входжень підстрічки в кожну введену стрічку");
-            if (choice == 2) {
-               /* int[] arr = new int[indexes.size()];
-                for (int i = 0; i < indexes.size(); i++) {
-                    arr[i] = indexes.get(i);
-                }
-                mergeSort(arr, arr.length);*/
-                for (int i = 0; i < indexes.size(); i++) {
-                    System.out.println("Кількість входжень підстрічки в " + (i + 1) + " стрічці: " + indexes.get(i));
-                }
-                break;
-            }
-            String str = DataInput.getString("Введіть стрічку: ").toLowerCase();
-            int count = 0;
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == pidStr.charAt(0) && i + pidStr.length() <= str.length()) {
-                    boolean isPid = true;
-                    for (int j = 0; j < pidStr.length(); j++) {
-                        if (str.charAt(i + j) != pidStr.charAt(j)) {
-                            isPid = false;
-                            break;
-                        }
-                    }
-                    if (isPid) {
-                        count++;
-                        i += pidStr.length() - 1;
-                    }
-                }
-            }
-            System.out.println("Кількість входжень: " + count);
-            indexes.add(count);
+        ArrayList<Integer> occurrences = new ArrayList<>();
+        String substring = DataInput.getString("Введіть шукану підстрічку: ").toLowerCase();
+        String[] strings = DataInput.getString("Введіть стрічки, розділені пробілами: ").toLowerCase().split("\\s+");
 
-
+        for (String inputString : strings) {
+            int count = countSubstringOccurrences(substring, inputString);
+            System.out.println("Кількість входжень підстрічки в стрічці " + inputString +": " + count);
+            occurrences.add(count);
+        }
+        sortOccurrences(occurrences);
+        System.out.println("Відсортовані кількості входжень:");
+        for (Integer occurrence : occurrences) {
+            System.out.println(occurrence);
         }
     }
-        public static void mergeSort(int[] a, int n) {
-            if (n < 2) {
-                return;
-            }
-            int mid = n / 2;
-            int[] l = new int[mid];
-            int[] r = new int[n - mid];
 
-            for (int i = 0; i < mid; i++) {
-                l[i] = a[i];
+    public static int countSubstringOccurrences(String substring, String inputString) {
+        int count = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            if (inputString.charAt(i) == substring.charAt(0) && i + substring.length() <= inputString.length()) {
+                boolean isSubstring = true;
+                for (int j = 0; j < substring.length(); j++) {
+                    if (inputString.charAt(i + j) != substring.charAt(j)) {
+                        isSubstring = false;
+                        break;
+                    }
+                }
+                if (isSubstring) {
+                    count++;
+                    i += substring.length() - 1;
+                }
             }
-            for (int i = mid; i < n; i++) {
-                r[i - mid] = a[i];
-            }
-            mergeSort(l, mid);
-            mergeSort(r, n - mid);
-
-            merge(a, l, r, mid, n - mid);
         }
-        public static void merge(int[] a, int[] l, int[] r, int left, int right) {
-            int i = 0, j = 0, k = 0;
-            while (i < left && j < right) {
-                if (l[i] <= r[j]) {
-                    a[k++] = l[i++];
+        return count;
+    }
+
+    public static void sortOccurrences(ArrayList<Integer> occurrences) {
+        for (int i = 0; i < occurrences.size() - 1; i++) {
+            for (int j = 0; j < occurrences.size() - i - 1; j++) {
+                if (occurrences.get(j) > occurrences.get(j + 1)) {
+                    int temp = occurrences.get(j);
+                    occurrences.set(j, occurrences.get(j + 1));
+                    occurrences.set(j + 1, temp);
                 }
-                else {
-                    a[k++] = r[j++];
-                }
-            }
-            while (i < left) {
-                a[k++] = l[i++];
-            }
-            while (j < right) {
-                a[k++] = r[j++];
             }
         }
     }
+}
